982838d1e2b6e558c5aee379cd1b16c9272c256d
Ph@ntom <586pboy@gmail.com>
2020-11-10 12:54:27 +0530

    Updates - 1 - added templates to the BaseApp. 2 - added IndexView to the views.py. 3 - added base.html and index.html.

--- GitDemo/GitDemo/urls.py
+++ GitDemo/GitDemo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from BaseApp import views
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name="index"),
     path('admin/', admin.site.urls),
 ]
